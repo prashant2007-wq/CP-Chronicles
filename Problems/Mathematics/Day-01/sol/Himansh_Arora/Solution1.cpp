018b0fcca688b85316d240dd6afcea80d36eb093
Humanshere <95287877+Humanshere@users.noreply.github.com>
2025-12-26 21:16:16 +0530

    Solved Mathematics/Day-01/q001.md

---
+++ Problems/Mathematics/Day-01/sol/Himansh_Arora/Solution1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int n=2*abs(a-b);
+
+    int opp=c+n/2;
+    if(c>n||b>n||a>n){
+        cout<<-1<<endl;
+        return;
+    }
+    if(opp>n)opp=opp-n;
+    
+    cout<<opp<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--)solve();
+}
